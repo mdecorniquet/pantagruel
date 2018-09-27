5d26b931d9b3517499534c4c02e9741f8fca51e0
Matthieu Decorniquet <matthieu.decorniquet@econocom.com>
2018-09-27 10:33:38 +0200

    pytest ok

--- tests/test_pantagruel.py
+++ tests/test_pantagruel.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-# def test_django():
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+def test_django():
+    browser = webdriver.Firefox()
+    browser.get('http://localhost:8000')
+    assert 'Django' in browser.title
