d51925ec1486499872360df6d360b604d5fea722
Matthieu Decorniquet <matthieu.decorniquet@econocom.com>
2018-09-27 17:24:14 +0200

    ajout input csv

--- tests/test_pantagruel.py
+++ tests/test_pantagruel.py
@@ -2,3 +2,7 @@ class TestFunctionalWeb:
     def test_app_is_pantagruel(self, browser):
         browser.get('http://localhost:8000')
         assert 'Pantagruel' in browser.title
+        header_text = browser.find_element_by_tag_name('h1').text
+        assert 'Import CSV' in header_text
+        inputbox = browser.find_element_by_id('id_new_item')
+        assert inputbox.get_attribute('placeholder') == 'Select a file'
