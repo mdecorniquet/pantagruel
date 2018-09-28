f1d27dfe7c0f28490e175b5402a3e2fccab8dbfc
Matthieu Decorniquet <matthieu.decorniquet@econocom.com>
2018-09-28 15:22:38 +0200

    add some func tests

--- tests/test_pantagruel.py
+++ tests/test_pantagruel.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+
 class TestFunctionalWeb:
     def test_app_is_pantagruel(self, browser):
         browser.get('http://localhost:8000')
@@ -6,3 +8,9 @@ class TestFunctionalWeb:
         assert 'Import CSV' in header_text
         inputbox = browser.find_element_by_id('id_new_item')
         assert inputbox.get_attribute('placeholder') == 'Select a file'
+        inputbox.send_keys('toto.csv')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        assert any(row.text == 'HOST' for row in rows)
